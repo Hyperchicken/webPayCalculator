3bae3275f1c394ad73b21233b794d575d4b05f6a
Hyperchicken <petar@hyperchicken.com>
2018-12-19 00:52:08 +1100

    main design and basic scripts added

---
+++ scripts.js
@@ -0,0 +1,25 @@
+//window.onload(alert("test"));
+
+function inputChanged() {
+    updateResults();
+}
+
+function updateResults() {
+    var resultArea = document.getElementById("result-area");
+    var testResult = addAllFields();
+
+    resultArea.innerHTML = "<p> Adding all the time fields gets you: " + testResult + "</p>";
+}
+
+
+function addAllFields() {
+    var times = document.querySelectorAll("input.time");
+    var total = 0;
+
+    var x;
+    for (x in times) {
+        var value = parseInt(times[x].value)
+        if(!isNaN(value)) total += value;
+    }
+    return total;
+}
