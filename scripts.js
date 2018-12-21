1628b851e2575d84c8ad276694c918dcd7dfdb29
hyperchicken <petar@hyperchicken.com>
2018-12-22 03:58:30 +1100

    started hours calculatios

--- scripts.js
+++ scripts.js
@@ -1,7 +1,47 @@
 "use strict";
 
+class Shift {
+    constructor(signOn, signOff) {
+        this.startHour = parseInt(signOn.substring(0,2));
+        this.startMinute = parseInt(signOn.substring(2,4));
+        this.endHour = parseInt(signOff.substring(0,2));
+        this.endMinute = parseInt(signOff.substring(2,4));
+    }
+
+    get hoursString(){
+        return this.calcHoursString();
+    }
+
+    calcHoursString() {
+        var timeWorked = 0;
+        var hours = this.endHour - this.startHour;
+        var minutes = this.endMinute - this.startMinute;
+        if(hours < 0) hours += 24;
+        if(minutes < 0) minutes += 60;
+        return hours + "h" + minutes + "m";
+    }
+}
+
 function inputChanged() {
     updateResults();
+    updateHoursPerShift();
+}
+
+function updateHoursPerShift() {
+    var times = document.querySelectorAll(".time");
+    var hoursField = document.querySelectorAll(".shift-hours");
+    var shifts = [];
+    for(var i = 0; i < times.length; i += 2) {
+        if(times[i].textLength == 4 && times[i+1].textLength == 4 && times[i].checkValidity() && times[i+1].checkValidity()){
+            var shift = new Shift(times[i].value, times[i+1].value);
+            console.log("Hours for i=" + i + ": " + shift.hoursString);
+            //console.log("Start hr: " + shift.startHour + " Start min: " + shift.startMinute + " End hr: " + shift.endHour + " End min: " + shift.endMinute);
+            shifts.push(shift); //TEMP: this will only work with shifts in sequence
+        }
+    }
+    for(var i = 0; i < shifts.length; i++) {
+        hoursField[i].innerHTML = shifts[i].hoursString;
+    }
 }
 
 function updateResults() {
