496f8030a4310769080b005873fd7a761a424d51
Hyperchicken <petar@hyperchicken.com>
2020-02-06 01:55:58 +1100

    changed html to php. added cache control

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+  //set headers to NOT cache a page
+  header("Cache-Control: no-cache, must-revalidate"); //HTTP 1.1
+  header("Pragma: no-cache"); //HTTP 1.0
+  header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+
+  //or, if you DO want a file to cache, use:
+  //header("Cache-Control: max-age=2592000"); //30days (60sec * 60min * 24hours * 30days)
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
