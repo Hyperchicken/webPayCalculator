e05f6d941d11770a338386f7504d1ab1c933f7af
Petar Stankovic <petar@hyperchicken.com>
2020-04-07 21:42:07 +1000

    version 1.09

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const calcVersion = "1.08";
-const calcLastUpdateDate = "26/02/2020";
+const calcVersion = "1.09";
+const calcLastUpdateDate = "07/04/2020";
 
 //rates
 const rateDates =               ["2018-01-01", "2018-07-01", "2019-01-01"];
