354ccb81f498e020fc2b075aa8104e4edce92ccf
Petar Stankovic <Petar.Stankovic@ad.etdsc.org.au>
2020-06-03 12:07:32 +1000

    added info for queens bday PH

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     <link rel="manifest" href="site.webmanifest">
     <script src="jquery.js"></script> 
     <script src="jquery-ui.js"></script>
-    <script src="publicHolidays.js?v1.01"></script> 
+    <script src="publicHolidays.js?v1.02"></script> 
     <script src="scripts.js?v=1.14"></script> 
     <script defer src="icons/font-awesome-all.js"></script> 
 </head>
