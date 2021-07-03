719616212ccd88eb206d61ccd7be8fcc5f94d0b7
Petar Stankovic <petar@hyperchicken.com>
2021-07-03 11:22:49 +1000

    add new file

---
+++ grades.js
@@ -0,0 +1,67 @@
+const grades = {
+    "spot" : {
+        "name" : "SPOT",
+        "ordinaryDays" : "10",
+        "ordinaryHours" : "8"
+    },
+    "level1" : {
+        "name" : "TD Level 1",
+        "ordinaryDays" : "10",
+        "ordinaryHours" : "8"
+    },
+    "trainee" : {
+        "name" : "Trainee",
+        "ordinaryDays" : "10",
+        "ordinaryHours" : "7.6"
+    },
+    "conversion" : {
+        "name" : "Conversion",
+        "ordinaryDays" : "10",
+        "ordinaryHours" : "8"
+    },
+    "parttime" : {
+        "name" : "Part-Time (SPOT)",
+        "ordinaryDays" : "10",
+        "ordinaryHours" : "7.6"
+    },
+    "jobshare" : {
+        "name" : "Job-Share (SPOT)",
+        "ordinaryDays" : "10",
+        "ordinaryHours" : "8"
+    },
+    "so8" : {
+        "name" : "TSO SO-8",
+        "ordinaryDays" : "10",
+        "ordinaryHours" : "8"
+    },
+    "so9" : {
+        "name" : "TSO SO-9",
+        "ordinaryDays" : "10",
+        "ordinaryHours" : "8"
+    },
+    "so10" : {
+        "name" : "TSO SO-10",
+        "ordinaryDays" : "10",
+        "ordinaryHours" : "8"
+    },
+    "so11" : {
+        "name" : "TSO SO-11",
+        "ordinaryDays" : "10",
+        "ordinaryHours" : "8"
+    },
+    "so12" : {
+        "name" : "TSO SO-12",
+        "ordinaryDays" : "10",
+        "ordinaryHours" : "8"
+    },
+    "dao" : {
+        "name" : "DAO",
+        "ordinaryDays" : "10",
+        "ordinaryHours" : "8"
+    },
+    "daoteamleader" : {
+        "name" : "DAO Team Leader",
+        "ordinaryDays" : "9",
+        "ordinaryHours" : "8.5"
+    }
+}
