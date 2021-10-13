00816a29763ba0e12258b6f916199f60f025baf1
Petar Stankovic <petar@hyperchicken.com>
2021-10-14 02:40:56 +1100

    added EA designation

--- grades.js
+++ grades.js
@@ -4,90 +4,103 @@ const grades = {
         "ordinaryDays" : 10,
         "ordinaryHours" : 8,
         "ddo" : true,
-        "suburbanAllowance" :  true
+        "suburbanAllowance" :  true,
+        "enterpriseAgreement": "drivers"
     },
     "level1" : {
         "name" : "TD Level 1",
         "ordinaryDays" : 10,
         "ordinaryHours" : 8,
         "ddo" : true,
-        "suburbanAllowance" :  true
+        "suburbanAllowance" :  true,
+        "enterpriseAgreement": "drivers"
     },
     "trainee" : {
         "name" : "Trainee",
         "ordinaryDays" : 10,
         "ordinaryHours" : 7.6,
         "ddo" : false,
-        "suburbanAllowance" :  false
+        "suburbanAllowance" :  false,
+        "enterpriseAgreement": "drivers"
     },
     "conversion" : {
         "name" : "Conversion",
         "ordinaryDays" : 10,
         "ordinaryHours" : 8,
         "ddo" : true,
-        "suburbanAllowance" :  true
+        "suburbanAllowance" :  true,
+        "enterpriseAgreement": "drivers"
     },
     "parttime" : {
         "name" : "Part-Time (SPOT)",
         "ordinaryDays" : 10,
         "ordinaryHours" : 7.6,
         "ddo" : false,
-        "suburbanAllowance" :  true
+        "suburbanAllowance" :  true,
+        "enterpriseAgreement": "drivers"
     },
     "jobshare" : {
         "name" : "Job-Share/FWA (SPOT)",
         "ordinaryDays" : 10,
         "ordinaryHours" : 8,
         "ddo" : false,
-        "suburbanAllowance" :  true
+        "suburbanAllowance" :  true,
+        "enterpriseAgreement": "drivers"
     },
     "so8" : {
         "name" : "TSO SO-8",
         "ordinaryDays" : 10,
         "ordinaryHours" : 8,
         "ddo" : true,
-        "suburbanAllowance" :  false
+        "suburbanAllowance" :  false,
+        "enterpriseAgreement": "drivers"
     },
     "so9" : {
         "name" : "TSO SO-9",
         "ordinaryDays" : 10,
         "ordinaryHours" : 8,
         "ddo" : true,
-        "suburbanAllowance" :  false
+        "suburbanAllowance" :  false,
+        "enterpriseAgreement": "drivers"
     },
     "so10" : {
         "name" : "TSO SO-10",
         "ordinaryDays" : 10,
         "ordinaryHours" : 8,
         "ddo" : true,
-        "suburbanAllowance" :  false
+        "suburbanAllowance" :  false,
+        "enterpriseAgreement": "drivers"
     },
     "so11" : {
         "name" : "TSO SO-11",
         "ordinaryDays" : 10,
         "ordinaryHours" : 8,
         "ddo" : true,
-        "suburbanAllowance" :  false
+        "suburbanAllowance" :  false,
+        "enterpriseAgreement": "drivers"
     },
     "so12" : {
         "name" : "TSO SO-12",
         "ordinaryDays" : 10,
         "ordinaryHours" : 8,
         "ddo" : true,
-        "suburbanAllowance" :  false
+        "suburbanAllowance" :  false,
+        "enterpriseAgreement": "drivers"
     },
     "dao" : {
         "name" : "DAO",
         "ordinaryDays" : 10,
         "ordinaryHours" : 8,
         "ddo" : true,
-        "suburbanAllowance" :  false
+        "suburbanAllowance" :  false,
+        "enterpriseAgreement": "operations"
     },
     "daoteamleader" : {
         "name" : "DAO Team Leader",
         "ordinaryDays" : 7,
         "ordinaryHours" : 8.5,
         "ddo" : false,
-        "suburbanAllowance" :  false
+        "suburbanAllowance" :  false,
+        "enterpriseAgreement": "operations"
     }
 }
