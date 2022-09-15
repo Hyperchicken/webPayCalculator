75b80ce270c30613a01d021374049976ed3f0f52
Petar Stankovic <petar@hyperchicken.com>
2022-09-16 02:47:58 +1000

    beta3

--- scripts.js
+++ scripts.js
@@ -7,8 +7,8 @@
 "use strict";
 
 //version
-const calcVersion = "1.37 BETA v2";
-const calcLastUpdateDate = "04/09/2022";
+const calcVersion = "1.37 BETA v3";
+const calcLastUpdateDate = "16/09/2022";
 
 //message of the day. topHelpBox message that appears once per calcVersion.
 //set to blank string ("") to disable message of the day
@@ -17,7 +17,9 @@ var motd = "Calculator updated to version " + calcVersion + " on " + calcLastUpd
 + "<li>Station Staff Update Beta</li>"
 + "<li>Please do not share this version of the Pay Calculator. It is not yet ready to be released.</li>"
 + "<li>Please send feedback, discrepancies and bug reports to <a href='mailto:petar@hyperchicken.com'>petar@hyperchicken.com</a> or call/message me on 0425158149. Thanks! - Petar</li>"
-+ "<li>BETA v2 - improved part-time stations overtime calculation</li>"
++ "<li>BETA v2 - improved part-time stations overtime calculation.</li>"
++ "<li>BETA v3 - part-time stations sick and public holiday credit now calculates based on the shift time entered.</li>"
++ "<li>BETA v3 - higher duties now automatically selects the last selected grade.</li>"
 + "</ul>";
 
 //colours
