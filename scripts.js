ddad1e63fe06249eb9bda8cb91f01abf1e413479
Petar Stankovic <petar@hyperchicken.com>
2024-03-24 21:23:52 +1100

    arvo shift allowance

--- scripts.js
+++ scripts.js
@@ -3220,11 +3220,13 @@ a PH or weekend get the increased rate. Extended shift is OT pay code. Rostered 
                         if(s.startHour == 4 || (s.startHour == 5 && s.startMinute <= 30)) { //early shift
                             shiftPay[day].push(new PayElement("earlyShift", shiftworkHours, day, rateTables, false));
                         }
-                        /*if(s.startHour < 18 && (s.endHour48 > 18 || (s.endHour48 == 18 && s.endMinute >= 30))) { //afternoon shift
-                            shiftPay[day].push(new PayElement("afternoonShift", shiftworkHours, day, rateTables, false));
-                        }*/
-                        if(s.startHour < 18 && (s.startHour + shiftPayGrade.ordinaryHours > 18 || (s.startHour + shiftPayGrade.ordinaryHours == 18 && Math.min(s.startMinute, s.endMinute) >= 30))) { //afternoon shift
-                            shiftPay[day].push(new PayElement("afternoonShift", shiftworkHours, day, rateTables, false));
+                        if(s.startHour < 18) { //afternoon shift
+                            if(s.hoursDecimal <= 8 && (s.endHour48 > 18 || (s.endHour48 == 18 && s.endMinute >= 30))) {
+                                shiftPay[day].push(new PayElement("afternoonShift", shiftworkHours, day, rateTables, false));
+                            }
+                            if(s.hoursDecimal > 8 && ((s.startHour + 8) > 18 || ((s.startHour + 8) == 18 && s.startMinute >= 30))) {
+                                shiftPay[day].push(new PayElement("afternoonShift", shiftworkHours, day, rateTables, false));
+                            }
                         }
                         if((s.startHour >= 18 && s.startHour <= 23) || (s.startHour >= 0 && s.startHour <= 3)) { //night shift
                             shiftPay[day].push(new PayElement("nightShift", shiftworkHours, day, rateTables, false));
